d98915f544b19ac5f19ff2d91649a0428eb31081
Zoe Zachary <zoezachary27@gmail.com>
2019-10-09 14:19:35 +0700

    uploading angkaPrima.js

---
+++ w4/angkaPrima.js
@@ -0,0 +1,12 @@
+function angkaPrima(angka) {
+  for(var i = 2; i <= Math.floor(Math.sqrt(angka)); i++) if(angka%i == 0) return false;
+
+  return true;
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
