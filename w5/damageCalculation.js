fa483fbc9f3aea85387e2b921c78cb996c2d2baa
Zoe Zachary <zoezachary27@gmail.com>
2019-10-17 16:25:13 +0700

    uploading damageCalculation.js

---
+++ w5/damageCalculation.js
@@ -0,0 +1,20 @@
+function attack (damage) {
+  return damage-2;
+}
+
+function damageCalculation (numberOfAttacks, damagePerAttack) {
+  var totalDmg = 0;
+
+  for(var i = 0; i < numberOfAttacks; i++) {
+    totalDmg += attack(damagePerAttack);
+  }
+
+  return totalDmg;
+}
+
+// TEST CASE
+console.log(damageCalculation(9, 25)); // 207
+
+console.log(damageCalculation(10, 4)); // 20
+
+console.log(damageCalculation(5, 20)); // 90
